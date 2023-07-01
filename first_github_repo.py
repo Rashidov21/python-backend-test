20f97863fe33c59f49b7d23f66ed413ed76871c3
Cutecoder <115334984+Cutecoder@users.noreply.github.com>
2023-07-01 15:00:36 +0500

    Update first_github_repo.py

--- first_github_repo.py
+++ first_github_repo.py
@@ -1,15 +1,1 @@
 # no desc
-
-#01.07.2023 - homework
-# a = int(input("start:"))
-# b = int(input("end:"))
-# answer = ''
-# if a>=0 and b<=30:
-#     for i in range(a,b+1):
-#         if i % 2 == 0:
-#             answer+=f'{i}  '
-#         else:
-#             answer+=f'{i} '
-# else:
-#     print("soningiz 0-dan kichkina yoki 30-dan katta")
-# print(answer)
