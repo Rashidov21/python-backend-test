651e5a483f5878a5ee72afa640989a86b3da2edc
Cutecoder <115334984+Cutecoder@users.noreply.github.com>
2023-07-01 14:57:21 +0500

    Update hamidullo.py

--- hamidullo.py
+++ hamidullo.py
@@ -1,1 +1,12 @@
-print("")
+# a = int(input("start:"))
+# b = int(input("end:"))
+# answer = ''
+# if a>=0 and b<=30:
+#     for i in range(a,b+1):
+#         if i % 2 == 0:
+#             answer+=f'{i}  '
+#         else:
+#             answer+=f'{i} '
+# else:
+#     print("soningiz 0-dan kichkina yoki 30-dan katta")
+# print(answer)
