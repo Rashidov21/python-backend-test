81cd5e74ae7a96f9993a28704c997917e7efc44e
XalililloMuhibbillayev <netlifyandgithub@gmail.com>
2023-07-01 14:55:26 +0500

    Homework

---
+++ xalilillo_python_code.py
@@ -0,0 +1,9 @@
+# a = int(input("Number :"))
+# ans = ""
+# if 0 < a < 30:
+#     for i in range(a):
+#         if i % 2 == 0:
+#             ans += f"{i}  "
+#         else:
+#             ans += f"{i} "
+# print(ans)
