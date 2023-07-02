bb727794411d1720b5dd95d3748e5b0e095ec667
abdullayev23 <muhammadqodir.org@gmail.com>
2023-07-02 22:55:05 +0500

    Update homeworks.py
    uy vazifa 2-masala

--- muhammadqodir_A/homeworks.py
+++ muhammadqodir_A/homeworks.py
@@ -1,1 +1,21 @@
-# test 
+# task - 1
+# a=int(input('qatorlar soni: '))
+# n=0
+# s=0
+# while n<a:
+#     for i in range(1):
+#         if s==0:
+#             print('*  #  *')
+#             s=1
+#         else:
+#             print('#  *  #')
+#             s=0
+#     n+=1 
+
+# task - 2
+# a = input("Matn kiriting: ")
+
+# if a.isalpha():
+#     print("Yaxshi")
+# else :
+#     print("iltimos matn kiriting!")
