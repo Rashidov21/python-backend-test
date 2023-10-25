b01c30065657941619d09e8b1821cd15a783f360
abdullayev23 <muhammadqodir.org@gmail.com>
2023-10-25 19:10:32 +0500

    test
    test

---
+++ eem66.txt
@@ -0,0 +1,1 @@
+"<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n    <meta charset=\"UTF-8\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <title>Document</title>\n</head>\n<body>\n    <h1>salom</h1>\n</body>\n</html>"
--- muhammadqodir_A/homeworks.py
+++ muhammadqodir_A/homeworks.py
@@ -1,22 +1,6 @@
-# task - 1
-# a=int(input('qatorlar soni: '))
-# n=0
-# s=0
-# while n<a:
-#     for i in range(1):
-#         if s==0:
-#             print('*  #  *')
-#             s=1
-#         else:
-#             print('#  *  #')
-#             s=0
-#     n+=1 
 
-# task - 2
-# a = input("Matn kiriting: ")
-
-# if a.isalpha():
-#     print("Yaxshi")
-# else :
-#     print("iltimos matn kiriting!")
-print(5)
+ints = 1
+floats = 1.2
+strs = 'salom'
+boolens = True
+print(ints,'\n',floats,'\n',strs,'\n',boolens)
