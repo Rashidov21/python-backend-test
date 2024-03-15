258ec115287491b7b742635da23873ae983833b4
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-03-15 16:28:30 +0500

    Create tasks.py

---
+++ tasks.py
@@ -0,0 +1,37 @@
+# for loop tasks
+
+# k = 10
+# n = 5
+
+# for i in range(n):
+#     print(k)
+    
+# task 2 
+# a = int(input("A ni kirit"))
+# b = int(input("B ni kirit"))
+# count = 0
+# if a < b:
+#     for i in range(a,b+1):
+#         print(i)
+#         count += 1
+#     print("Barcha sonlar ", count)
+#     print("Barcha sonlar len orqali ", len(range(a,b+1)))
+
+# task 4
+# price = 15000
+# for i in range(1,11):
+#     print(i * price)
+
+# task 5 
+# userdan matn qabul qiling toki "z" harfi topilmagunicha barcha harflarini ekranga chiqaring
+# input >> 'keldizku'
+# output >> k,e,l,d,i,z
+
+text = input("Matn kirit \n")
+i = 0
+while i <= (len(text) - 1):
+    print(text[i])
+    i += 1
+    if text[i] == "z":
+        break
+    
