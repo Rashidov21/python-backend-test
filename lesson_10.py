73c49c0ec0e1ed88f76cde78de984bc187be15f7
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-03-20 16:26:35 +0500

    Create lesson_10.py

---
+++ lesson_10.py
@@ -0,0 +1,57 @@
+# list - ro'yhatlar ma'lumotlar tuzilmasi 
+# list - tartibli elementlar toplami 
+# l = list("python")
+# print(l) # ['p', 'y', 't', 'h', 'o', 'n']
+# print(l[0]) # p
+# print(len(l)) # 6
+
+
+# l = [1,2,3,4,5]
+# print(l)
+
+# list - data structure
+# arr = [1,"salom",True,None,[1,2,3, [4,5,6]]]
+# print(arr[-1]) # [1, 2, 3]
+# print(arr[-1][3][1]) # 5
+
+# arr = [1,2,3,4,5]
+# print(arr[10]) # IndexError: list index out of range
+
+# a,b,c = 1,2,3
+# x,y,*z = [1,2,3,4,5]
+# print(x,y,z) # 1 2 [3, 4, 5]
+
+# arr = [1,2,3,4,5]
+# for i in arr:
+#     print(i)
+    
+# mat = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+
+# for i in mat:
+#    print(*i)
+   
+# print(*[1,2,3])
+
+# task 
+# arr = [1,2,3,4,5,6,7,8,9,10]
+
+# a = []
+# for i in range(1000):
+#     a.append(i)
+# print(a)
+
+# for i in list(range(100)):print(i,end=" ")
+
+# task 
+# 1 dan 10 gacha bolgan sonlarni kvadratlaridan iborat list hosil qiling 
+
+arr = []
+for i in range(1,11):
+    arr.append(i ** 2)
+print(arr) # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+print([x ** 2 for x in range(1,11)]) # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
