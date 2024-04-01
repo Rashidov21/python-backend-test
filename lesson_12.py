2e26f550e1f8dea3ee8749bba4816c937faf2559
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2024-04-01 16:27:17 +0500

    Update lesson_12.py

--- lesson_12.py
+++ lesson_12.py
@@ -62,14 +62,81 @@ person = {
 
 # task 
 # input:
-a = "abcdefgh"
-b = list(range(1,9))
+# a = "abcdefgh"
+# b = list(range(1,9))
 # output:
 # d = {"a":1,"b":2...}
-c = {}
-for i in range(len(a)):
-    c.setdefault(a[i],b[i])
+# c = {}
+# for i in range(len(a)):
+#     c.setdefault(a[i],b[i])
     
-print(c) # {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
+# print(c) # {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
 
-print({k:v for k,v in zip(a,b)}) # {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
+# print({k:v for k,v in zip(a,b)}) # {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
+
+# pop - korsatilgan kalit ostidagi elementi olish kalit berilmasa xatolik vujudga keladi 
+# d = {"a":1,"b":2}
+# d.pop("a") # {'b': 2}
+# d.pop() # TypeError
+# print(d)
+# popitem - tasodify elementi ochirish va uni tuple korinishida qaytarish
+# print(d.popitem())
+
+# update - dictga yangi elemnt qoshish 
+# d.update(c=3)
+# d.update({'d':4})
+# print(d) # {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+# d["e"] = 5
+# print(d) # {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+
+# copy - nusxa olish 
+# d_copy = d.copy()
+# print(d_copy) # {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+
+# new_dict = dict.fromkeys("abc",[1,2,3])
+# print(new_dict) # {'a': [1, 2, 3], 'b': [1, 2, 3], 'c': [1, 2, 3]}
+# print(dict.fromkeys("abc")) #{'a': None, 'b': None, 'c': None}
+# print(dict.fromkeys("abc",True)) #{'a': True, 'b': True, 'c': True}
+
+# get 
+# setdefault 
+# pop 
+# popitem
+# clear
+# copy 
+# update
+# fromkeys
+# in 
+# not in 
+
+# d = {"a":1,"b":2}
+# print("a" in d) # True
+# print("a" not in d) # False
+
+# task 
+products = [
+        {
+        "name":"Zelen",
+        "price":1500
+        },
+        {
+        "name":"Cola",
+        "price":15000
+        },
+        {
+        "name":"Non",
+        "price":3500
+        },
+        {
+        "name":"Ukrop",
+        "price":1500
+        },
+        
+]
+summa = 0
+for i in products:
+    summa += i.get("price")
+print(summa)
+# Ushbu 
+# dictdagi barcha tovarlarni umumiy summasini hisoblovchi dastur tuzing
